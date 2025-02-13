df19e259a1ef5103ecda7ee022ebe0612adc14cc
Davi <bessadavi04@gmail.com>
2025-02-12 21:21:21 -0300

    Ola

---
+++ src/Exercicio2.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Exercicio2 {
+    public static void main(String[] args) {
+        Scanner ler = new Scanner(System.in);
+
+        System.out.print("Digite seu nome: ");
+        String nome = ler.nextLine();
+        System.out.println("Olá "+ nome+", é um prazer te conhecer!");
+    }
+}
