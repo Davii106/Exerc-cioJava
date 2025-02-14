1c5e8e2d72a2f9d6da238befb80af24d57b88577
Davi Bessa Araujo <2400899@SALAS.AULAS>
2025-02-14 19:42:22 -0300

    Ola

---
+++ src/Exercicio5.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Exercicio5 {
+    public static void main(String[] args) {
+        Scanner ler = new Scanner(System.in);
+
+        System.out.println("Digite a sua primeira nota:");
+        float nota1 = ler.nextFloat();
+        System.out.println("Digite a sua segunda nota:");
+        float nota2 = ler.nextFloat();
+
+        float media = (nota1 + nota2)/2;
+        System.out.println("A média entre "+ nota1+" e "+ nota2+" é igual a "+media);
+
+    }
+}
