d0a4230cd25b54c52468e90a470d01e1e1a82c10
Davi Bessa Araujo <2400899@SALAS.AULAS>
2025-02-14 20:34:05 -0300

    Ola

---
+++ src/Exercicio10.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Exercicio10 {
+    public static void main(String[] args) {
+        Scanner ler = new Scanner (System.in);
+
+        System.out.print("Digite a altura em metros:");
+        double altura = ler.nextDouble();
+        System.out.print("Digite a comprimento metros:");
+        double comprimento = ler.nextDouble();
+        double area = altura*comprimento;
+        double litros_de_tinta = area*2;
+
+        System.out.print("Você precisará de "+litros_de_tinta+" litros para pintar uma "+area+" metros");
+    }
+}
