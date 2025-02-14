5dad66859c0f9722796440abb5370daee769b178
Davi Bessa Araujo <2400899@SALAS.AULAS>
2025-02-14 20:19:40 -0300

    Ola

---
+++ src/Exercicio9.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Exercicio9 {
+    public static void main(String[] args) {
+        Scanner ler = new Scanner(System.in);
+
+        System.out.print("Digite quanto você possui na carteira em R$:");
+        double real = ler.nextDouble();
+        double dolar = real/6.09;
+
+        System.out.println("Você tem "+ real+" e pode comprar "+dolar);
+    }
+}
