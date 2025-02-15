a8af36a54bb298eb9b712c51938334c74d0a16e9
Davi Bessa Araujo <2400899@SALAS.AULAS>
2025-02-14 21:31:20 -0300

    Ola

---
+++ src/Exercicio14.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Exercicio14 {
+    public static void main(String[] args) {
+        Scanner ler = new Scanner (System.in);
+
+        System.out.print("Digite quantos Km você percorreu com o carro:");
+        double kms = ler.nextDouble();
+        System.out.print("Digite a quantidade de dias que você alugou o carro:");
+        double dias= ler.nextDouble();
+        double preço_total = (90*dias)+(0.20*kms);
+
+        System.out.print("Você pagará um total de R$"+preço_total+" por ter usado o carro por "+dias+" dias e percorrido "+kms+" Km");
+    }
+}
