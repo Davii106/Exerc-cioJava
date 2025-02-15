ebe88750e324af2746b2f9059acc28272fbd2008
Davi Bessa Araujo <2400899@SALAS.AULAS>
2025-02-14 21:12:03 -0300

    Ola

---
+++ src/Exercicio12.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Exercicio12 {
+    public static void main(String[] args) {
+        Scanner ler = new Scanner (System.in);
+
+        System.out.print("Digite o valor de seu produto:");
+        float produto = ler.nextFloat();
+        double desconto = produto - (produto*(0.05));
+
+        System.out.print("Com o desconto de 5% seu "+produto+" ficará a "+desconto);
+    }
+}
