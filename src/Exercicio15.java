a8a64c2ce1ee62e6a549f203534efc90775b3ef3
Davi Bessa Araujo <2400899@SALAS.AULAS>
2025-02-14 21:39:44 -0300

    Ola

---
+++ src/Exercicio15.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Exercicio15 {
+    public static void main(String[] args) {
+        Scanner ler = new Scanner (System.in);
+
+        System.out.print("Digite o número de dias que você trabalhou neste mês:");
+        double dias = ler.nextDouble();
+        System.out.print("Digite a quantidade de horas que você trabalha em um dia:");
+        double horas = ler.nextDouble();
+        double salario = horas*25*dias;
+
+        System.out.print("O seu salário deste mês é R$"+salario+" por ter trabalhado "+horas+" horas e "+dias+" dias no mês");
+    }
+}
