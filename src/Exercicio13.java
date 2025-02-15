f7259d82f971f787ce8daffc8d84099c56f40348
Davi Bessa Araujo <2400899@SALAS.AULAS>
2025-02-14 21:22:15 -0300

    Ola

---
+++ src/Exercicio13.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Exercicio13 {
+    public static void main(String[] args) {
+        Scanner ler = new Scanner (System.in);
+
+        System.out.print("Digite seu salário:");
+        double salario = ler.nextDouble();
+        double novo_salario = salario+(salario*(0.15));
+
+        System.out.print("Seu antigo salário era "+salario+" e com o aumento de 15% ficará "+novo_salario);
+    }
+}
