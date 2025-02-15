7af287fb2820b7477dc7dd2f2d84492de1eca490
Davi Bessa Araujo <2400899@SALAS.AULAS>
2025-02-14 21:53:20 -0300

    Ola

---
+++ src/Exercicio16.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Exercicio16 {
+    public static void main(String[] args) {
+        Scanner ler = new Scanner (System.in);
+
+        System.out.print("Digite a quantidade de cigarros que você fuma no dia:");
+        double cigarros = ler.nextDouble();
+        System.out.print("Digite a quantos anos você já fuma:");
+        double anos_de_fumo = ler.nextDouble();
+        double perca_de_vida = (cigarros*10)/24;
+
+        System.out.print("Você perderá "+perca_de_vida+" a cada "+cigarros+" cigarro que fumar");
+    }
+}
